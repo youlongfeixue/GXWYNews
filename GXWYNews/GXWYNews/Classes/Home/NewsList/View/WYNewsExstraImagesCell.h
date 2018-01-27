d76a8f22f59f0232dacfbb2d52c6f4d528d9555b
tony <hehuanyilan@163.com>
2018-01-27 23:46:58 +0800

    多图cell 自动布局ok

---
+++ GXWYNews/GXWYNews/Classes/Home/NewsList/View/WYNewsExstraImagesCell.h
@@ -0,0 +1,19 @@
+//
+//  WYNewsExstraImagesCell.h
+//  GXWYNews
+//
+//  Created by Tony on 2018/1/27.
+//  Copyright © 2018年 Tony. All rights reserved.
+//
+
+#import "GXBaseTableViewCell.h"
+
+@interface WYNewsExstraImagesCell : GXBaseTableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *iconView;
+@property (weak, nonatomic) IBOutlet UILabel *lblTitle;
+@property (weak, nonatomic) IBOutlet UILabel *lblSource;
+@property (weak, nonatomic) IBOutlet UILabel *lblReply;
+@property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *extralIcon;
+
+@end
---
+++ GXWYNews/GXWYNews/Classes/Home/NewsList/View/WYNewsExstraImagesCell.m
@@ -0,0 +1,24 @@
+//
+//  WYNewsExstraImagesCell.m
+//  GXWYNews
+//
+//  Created by Tony on 2018/1/27.
+//  Copyright © 2018年 Tony. All rights reserved.
+//
+
+#import "WYNewsExstraImagesCell.h"
+
+@implementation WYNewsExstraImagesCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
