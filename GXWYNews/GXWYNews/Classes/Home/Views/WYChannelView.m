93f6e2d5cecf8d2dedb0561b3e9ffc95272841f4
tony <hehuanyilan@163.com>
2018-01-30 22:27:02 +0800

    频道视图中label的布局  设置frame布局

--- GXWYNews/GXWYNews/Classes/Home/Views/WYChannelView.m
+++ GXWYNews/GXWYNews/Classes/Home/Views/WYChannelView.m
@@ -30,11 +30,27 @@
 - (void)setChannelList:(NSArray<WYChannelModel *> *)channelList {
     _channelList = channelList;
     
+    CGFloat x = 20;
+    CGFloat margin = 8;
+    CGFloat height = _scrollView.bounds.size.height;
+    // width 已经 sizeToFit 计算好了 - 撑开了。
+    
     // add subControl 2 scrollView
     for (WYChannelModel *channel in _channelList) {
         WYChannelLabel *lb = [WYChannelLabel channelLabelWithTitle:channel.tname];
+        
+        CGFloat width = lb.bounds.size.width;
+        lb.frame = CGRectMake(x, 0, width, height);
+        
+        x += width + margin;
+        
         [_scrollView addSubview:lb];
     }
+    
+    _scrollView.contentSize = CGSizeMake(x, 0);
+    
+    _scrollView.showsVerticalScrollIndicator = NO;
+    _scrollView.showsHorizontalScrollIndicator = NO;
 }
 
 
