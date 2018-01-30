034e84c586b55c8c0dc243efe7471c6558dfd1db
tony <hehuanyilan@163.com>
2018-01-30 22:59:16 +0800

    添加分页视图控制器

--- GXWYNews/GXWYNews/Classes/Home/WYHomeViewController.m
+++ GXWYNews/GXWYNews/Classes/Home/WYHomeViewController.m
@@ -9,6 +9,7 @@
 #import "WYHomeViewController.h"
 #import "WYChannelView.h"
 #import "WYChannelModel.h"
+#import "WYNewsListController.h"
 
 @interface WYHomeViewController ()
 
@@ -60,6 +61,32 @@
         make.height.mas_equalTo(38);
     }];
     
+    // 设置分页控制器
+    [self setupPageController];
+}
+
+/// 设置分页控制器
+- (void)setupPageController {
+    // 1. 实例化分页控制器
+    UIPageViewController *pvc = [[UIPageViewController alloc] initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal options:nil];
+    
+    // 2. 设置分页控制器的子控制器 （新闻列表控制器）
+    WYNewsListController *listVc = [WYNewsListController new];
+    [pvc setViewControllers:@[listVc] direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:nil];
+    
+    // 3. 将分页视图控制器当做子控制器添加到当前控制器
+    [self addChildViewController:pvc];
+    
+    // 4. 添加视图，并布局
+    [self.view addSubview:pvc.view];
+    
+    [pvc.view mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.channelView.mas_bottom);
+        make.left.bottom.right.equalTo(self.view);
+    }];
+    
+    // 5. 完成添加
+    [pvc didMoveToParentViewController:self];
 }
 
 
